4669c6d1fbc275de00c6b437ba14c961af3fcccb
rld2804200-oss <rld2804200@gmail.com>
2026-08-10 22:37:46 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- TVWebHook_68.5_Port_BloFin.py
+++ TVWebHook_68.5_Port_BloFin.py
@@ -38,7 +38,7 @@ DEFAULTS = {
     "SUBACCOUNT_NAME": "BloFin Acc.",                   # Name of the sub-account (shows in every log)
     "EXCHANGE": "blofin",                          # Exchange name for CCXT
     "REFUSE_SYMBOLS": ["XRP/USD:USD"],
-    "FORWARD_REFUSED_TO": "https://ranbot.site/",
+    "FORWARD_REFUSED_TO": "https://bot.io/",
     "ALWAYS_FORWARD": False,                       # true = always forward every alert (duplicate)
     "ENABLE_FORWARDING": True,                     # master switch — set false to disable all forwarding
     "SYMBOL": "BTC/USD:USD",                       # Default symbol
